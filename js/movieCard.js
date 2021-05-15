88e89f291f8e522cfe7166372d572abdd77a0db8
david-sds <dave.sathler@gmail.com>
2021-05-15 18:38:52 -0300

    Sprint 3 - Funcional

--- js/movieCard.js
+++ js/movieCard.js
@@ -10,12 +10,29 @@ function fMontaCard() {
         dataType: "json",
         url: "../../php/montaCardFilmes.php",
         success: function(retorno){
+
+
             var cont = 0;
             for(i = 1; i <= 5; i++) {
                 for(j = 1; j <= 6; j++) {
                     var categoriaAtual = "#carolFilmes"
                     var filmeAtual = $(categoriaAtual + " section:nth-child("+i+") div:nth-child("+j+")");
                     if(cont < retorno.tamanhoFilmes) {
+                        filmeAtual.css("background-image", "url('../../images/thumbnails/"+retorno.filmes[cont][1]+".webp')");
+                        filmeAtual.attr("movieid", retorno.filmes[cont][0]);
+
+                    }
+                    cont++;
+                    filmeAtual.append(cont)
+                }
+            }   
+
+            var cont = 0;
+            for(i = 1; i <= 5; i++) {
+                for(j = 1; j <= 6; j++) {
+                    var categoriaAtual = "#carolSeries"
+                    var filmeAtual = $(categoriaAtual + " section:nth-child("+i+") div:nth-child("+j+")");
+                    if(cont < retorno.tamanhoSeries) {
                         filmeAtual.css("background-image", "url('../../images/thumbnails/"+retorno.series[cont][1]+".webp')");
                         filmeAtual.attr("movieid", retorno.series[cont][0]);
 
@@ -23,7 +40,25 @@ function fMontaCard() {
                     cont++;
                     filmeAtual.append(cont)
                 }
-            }     
+            }   
+
+            
+            var cont = 0;
+            for(i = 1; i <= 5; i++) {
+                for(j = 1; j <= 6; j++) {
+                    var categoriaAtual = "#carolFavoritos"
+                    var filmeAtual = $(categoriaAtual + " section:nth-child("+i+") div:nth-child("+j+")");
+                    if(cont < retorno.tamanhoFavoritos) {
+                        filmeAtual.css("background-image", "url('../../images/thumbnails/"+retorno.favoritos[cont][1]+".webp')");
+                        filmeAtual.attr("movieid", retorno.favoritos[cont][0]);
+
+                    }
+                    cont++;
+                    filmeAtual.append(cont)
+                }
+            }   
+
+            console.log(retorno.favoritos)
         } 
     });
 }
--- php/montaCardFilmes.php
+++ php/montaCardFilmes.php
@@ -3,10 +3,9 @@
     session_start();
     require "config.php";  
 
-    $contaFilmes = mysqli_query($con, "SELECT COUNT(*) FROM filmes");
-    $row1 = mysqli_fetch_row($contaFilmes);
-    $retorno["tamanhoFilmes"] = $row1[0];
+    $user_email = $_SESSION['mail'];
 
+    /* RETORNA VALORES DE CADA FILME/SERIE */
     $continuar = mysqli_query($con, "SELECT * FROM filmes WHERE tipo = 'filmes'");
     $row = mysqli_fetch_all($continuar);
     $retorno["filmes"] = $row;
@@ -14,6 +13,28 @@
     $continuar2 = mysqli_query($con, "SELECT * FROM filmes WHERE tipo = 'series'");
     $row2 = mysqli_fetch_all($continuar2);
     $retorno["series"] = $row2;
+
+    /* RETORNA FILMES FAVORITADOS */
+    $get_id_bd = mysqli_query($con, "SELECT id FROM usuarios WHERE cadMail = '$user_email'");
+    $fetch_id = mysqli_fetch_row($get_id_bd);
+    $user_id = $fetch_id[0];
+
+    $continuar10 = mysqli_query($con, "SELECT filmes.* FROM favoritos, filmes WHERE id_usuario = '$user_id' AND favoritos.id_filme = filmes.id");
+    $row10 = mysqli_fetch_all($continuar10);
+    $retorno["favoritos"] = $row10;
+
+    /* CONTA PARA CADA CAROUSEL */
+    $contaFilmes = mysqli_query($con, "SELECT COUNT(*) FROM filmes WHERE tipo = 'filmes'");
+    $rowFilmes = mysqli_fetch_row($contaFilmes);
+    $retorno["tamanhoFilmes"] = $rowFilmes[0];
+
+    $contaSeries = mysqli_query($con, "SELECT COUNT(*) FROM filmes WHERE tipo = 'series'");
+    $rowSeries = mysqli_fetch_row($contaSeries);
+    $retorno["tamanhoSeries"] = $rowSeries[0];
+    
+    $contaFavoritos = mysqli_query($con, "SELECT COUNT(*) FROM favoritos WHERE id_usuario = '$user_id'");
+    $rowFavoritos = mysqli_fetch_all($contaFavoritos);
+    $retorno["tamanhoFavoritos"] = $rowFavoritos[0];
 
     echo json_encode($retorno);
 ?>
